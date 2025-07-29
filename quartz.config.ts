95090b7a01b60a1203a2c2c321339ec87f3ddecb
Kanzu <55020589+Kanzu32@users.noreply.github.com>
2025-07-29 16:32:50 +0300

    emoji-fix

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "💀Kanzu portfolio",
+    pageTitle: "Kanzu portfolio",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: false,
